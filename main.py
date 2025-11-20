ae27de0ab7ff2d9a1a8e3440cbc222f9204e7d72
vasunavadiya90 <vasunavadiya923@gmail.com>
2025-11-20 18:03:09 +0530

    main branch sum changes

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-def sum(a,b):
-   return
-   a+b
+def sum(a,b,c):
+   return a+b+c
 
-s = sum(3,5)
+s = sum(3,5,2)
 print("sum of the elements is : " , s)
 
 
 def div(a,b):
     return a-b
-
-
 
 s = div(3,5)
 print("div of the elements is : " , s)

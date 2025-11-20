2f704082a7f1fc21da6a34d18d3628f57a2734c0
vasunavadiya90 <vasunavadiya923@gmail.com>
2025-11-20 17:56:08 +0530

    this is my developera commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def sum(a,b):
-    a+b
+   return
+   a+b
 
 s = sum(3,5)
 print("sum of the elements is : " , s)
+
+
+def div(a,b):
+    return a-b
+
+
+
+s = div(3,5)
+print("div of the elements is : " , s)

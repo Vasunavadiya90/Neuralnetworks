f05eec8e666b6513a97dedec28e0f251ac09746e
Vasunavadiya90 <vasunavadiya923@gmail.com>
2025-11-20 18:02:24 +0530

    Modify div function to accept three parameters

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ s = sum(3,5)
 print("sum of the elements is : " , s)
 
 
-def div(a,b):
-    return a-b
+def div(a,b,c):
+    return a-b-c
 
 
 
-s = div(3,5)
+s = div(3,5,5)
 print("div of the elements is : " , s)
